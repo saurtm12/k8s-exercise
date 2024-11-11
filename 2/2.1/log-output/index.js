ba1a4115271499c9a418c12fc8ab02df19aa2ea3
Duc Hong <hongnghiaduc12@gmail.com>
2024-11-11 10:35:21 +0200

    Add 2.2

---
+++ 2/2.1/log-output/index.js
@@ -0,0 +1,26 @@
+// index.js
+const { default: axios } = require('axios');
+const express = require('express');
+const app = express();
+const fs = require("fs")
+// Use PORT environment variable or default to 3000
+const port = process.env.PORT || 3000;
+const PINGPONG_SERVICE = process.env.PINGPONG_SERVICE || "http://localhost:3003";
+
+
+
+app.get('/', (req, res) => {
+    res.send('Log App Hello World!');
+});
+
+app.get('/log', async (req, res) => {
+        const upStreamRes = await axios.get(`${PINGPONG_SERVICE}/info`)
+        const data = upStreamRes.data;
+        res.send(`${data.timestamp}: ${Math.random().toString(36).substring(2, 36)}
+        Ping / Pongs: ${data.count}`)
+});
+
+// Start the server and log the port
+app.listen(port, () => {
+    console.log(`Server started in port ${port}`);
+});
